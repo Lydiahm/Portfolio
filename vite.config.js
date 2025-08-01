c2dbf4d78b2d1de6aea8a300083b687466efbbfa
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-08-01 02:23:27 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Spécifie le répertoire de sortie pour Vercel
+  }
 })
